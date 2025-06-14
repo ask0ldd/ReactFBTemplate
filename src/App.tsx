f1cf006f39fe23e50d6c09363c267790a71ded96
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-14 09:34:57 +0200

    *edit* front / firebase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {useState} from 'react';
 import './App.css'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from './firebase';
+
+// https://hackernoon.com/how-to-set-up-firebase-authentication-with-react
+// https://firebase.google.com/docs/auth
+// https://www.youtube.com/watch?v=WpIDez53SK4&t=2s
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  async function handleSubmit(event : React.FormEvent) {
+    event.preventDefault()
+    console.log('Login:', email);
+    console.log('Password:', password);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Login</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor="password" style={{ marginTop: '20px' }}>Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Submit" style={{ marginTop: '20px' }}/>
+      </form>
+    </main>
+  );
 }
 
 export default App
