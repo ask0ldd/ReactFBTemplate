0525474feceab034070cdbe905ebd583cc4c81e2
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-15 05:56:34 +0200

    *add* front / user interfaces

---
+++ src/services/auth/interfaces/IAuthUser.ts
@@ -0,0 +1,7 @@
+export default interface IAuthUser{
+    email : string | null
+    displayName : string | null
+    uid : string | null
+    refreshToken : string | null
+    accessToken : string | null
+}
