9cc2183a59c3d4f59a6782f48827edcce6575a94
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-17 06:44:55 +0200

    *edit* front / auth & services context integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ServicesProvider } from './contexts/ServicesContext.tsx'
+import { AuthProvider } from './contexts/AuthContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ServicesProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ServicesProvider>
   </StrictMode>,
 )
