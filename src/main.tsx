173364192f7f0fca19d87b04935706e4e15d1220
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-21 12:54:55 +0200

    *edit* front / routes definition

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,22 @@ import './index.css'
 import App from './App.tsx'
 import { ServicesProvider } from './contexts/ServicesContext.tsx'
 import { AuthProvider } from './contexts/AuthContext.tsx'
+import Login from './pages/Login.tsx'
+import Route from './router/Route.tsx'
+import { RouterProvider } from './router/RouterProvider.tsx'
+import Register from './pages/Register.tsx'
+import Testing from './pages/Testing.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ServicesProvider>
       <AuthProvider>
-        <App />
+        <RouterProvider base={"http://localhost:5173"}>
+          <Route path={"/"} element={<App />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Register />} />
+          <Route path={"/tests"} element={<Testing />} />
+      </RouterProvider>
       </AuthProvider>
     </ServicesProvider>
   </StrictMode>,
