c448c22b20715b381b6b48ee6bb7088290bb88ce
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-22 08:57:46 +0200

    *add* front / testing page

---
+++ src/pages/Testing.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Testing(){
+    return(
+        <div className="globalContainer">
+            <Header activeMenuItem="tests"/>
+            <main>
+            </main>
+        </div>
+    )
+}
