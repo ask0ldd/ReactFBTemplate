a1c93ca542cd200c4d83df6a64b67d6888d7a178
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-22 08:57:19 +0200

    *add* front / header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from "../router/Link";
+
+export default function Header({activeMenuItem} : {activeMenuItem : string}){
+    return(
+        <header>
+            <ul style={{display:'flex', flexDirection:'row', columnGap:'10px', listStyleType:'none'}}>
+                <Link href={"/login"}><li style={activeMenuItem == "login" ? {background:'#fff', width:'100px', textAlign:'center'} : {width:'100px', textAlign:'center'}}>login</li></Link>
+                <Link href={"/register"}><li style={activeMenuItem == "register" ? {background:'#fff', width:'100px', textAlign:'center'} : {width:'100px', textAlign:'center'}}>register</li></Link>
+                <Link href={"/tests"}><li style={activeMenuItem == "tests" ? {background:'#fff', width:'100px', textAlign:'center'} : {width:'100px', textAlign:'center'}}>tests</li></Link>
+            </ul>
+        </header>
+    )
+}
