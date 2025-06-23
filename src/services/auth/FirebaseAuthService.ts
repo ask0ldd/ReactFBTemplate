268ed0fe886334d9ec2ad219a98dc8f242924586
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-06-23 20:07:07 +0200

    *edit* front / firebase auth service : signInWithProvider

--- src/services/auth/FirebaseAuthService.ts
+++ src/services/auth/FirebaseAuthService.ts
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, type User, type UserCredential } from "@firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, type User, type UserCredential } from "@firebase/auth";
 import { auth } from "../../firebase";
 import type IAuthUser from "./interfaces/IAuthUser";
 import type { AuthService } from "./interfaces/AuthService";
@@ -36,5 +36,22 @@ export default class FirebaseAuthService implements AuthService{
             console.error(error)
             throw error
         }
+    }
+
+    async signInWithProvider(provider : GoogleAuthProvider){
+        try{
+            const result = await signInWithPopup(auth, provider)
+            const user = result.user;
+            console.log(JSON.stringify(user))
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        }catch(error : any){
+            // const errorCode = error.code;
+            if (error?.message) {
+                console.error("Sign-in error:", error.message);
+            } else {
+                console.error("Unknown sign-in error:", error);
+            }
+        }
+
     }
 }
